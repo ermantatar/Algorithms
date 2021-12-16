9ab32298af54183980813ba88286b4cd4c709779
Erman Tatar <ermantatar@Ermans-MacBook-Pro.local>
2021-12-16 12:32:00 -0500

    first question batch

---
+++ Contains_Duplicates_217.java
@@ -0,0 +1,36 @@
+// Question: https://leetcode.com/problems/contains-duplicate/
+
+// Brute Force 
+// Time Complexity: O(N^2)
+// Space Compleity: O(1)
+public boolean containsDuplicate(int[] nums) {
+    for (int i = 0; i < nums.length; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (nums[j] == nums[i]) return true;  
+        }
+    }
+    return false;
+}
+
+
+// Sorting
+// Time Complexity: O(NlogN)
+// Space Complexity: O(1)
+public boolean containsDuplicate(int[] nums) {
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length - 1; ++i) {
+        if (nums[i] == nums[i + 1]) return true;
+    }
+    return false;
+}
+
+// HashSet
+public boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>(nums.length);
+    for (int x: nums) {
+        if (set.contains(x)) return true;
+        set.add(x);
+    }
+    return false;
+}
+
